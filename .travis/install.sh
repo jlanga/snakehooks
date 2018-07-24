db216d3ffdccb85192a1ff264149b1a6391556a4
Jorge Langa <jorge.langa.arranz@gmail.com>
2018-07-24 11:43:43 +0200

    Deleting miniconda folder because cacher creates it

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,6 +10,7 @@ if [ ! -d "$HOME"/miniconda/bin ]; then
         --continue \
         https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
         --output-document=miniconda.sh
+    rm -rf "$HOME"/miniconda
     bash miniconda.sh -b -p "$HOME"/miniconda
     export PATH="$HOME/miniconda/bin:$PATH"
     hash -r
